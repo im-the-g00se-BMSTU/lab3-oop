603e5fbbd54a903127661d1c43f76dfa4e02bfd3
Max Semionov <semenov.max.art@mail.ru>
2026-05-29 20:39:05 +0300

    chore: убрать ненужные директории из отслеживания

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab3_FILE C:/Users/User/Documents/Qt/OOP/module2/lab3/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/lab3.exe)
-set(__QT_DEPLOY_TARGET_lab3_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_lab3_RUNTIME_DLLS C:/Programs/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Programs/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Programs/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
